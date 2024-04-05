50c345c37faef71e4f47510b34565b32528f3bda
ArGuto <argo.wahyu.utomo@gmail.com>
2024-04-05 14:10:53 +0700

    live coding feedback result: models.py related_name="user"

--- blog/models.py
+++ blog/models.py
@@ -19,7 +19,7 @@ class Tag(models.Model):
 # User has a One to many relationship with Post
 # One user can have multiple post
 class Post(models.Model):
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="user")
     title = models.CharField(max_length=200)
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
